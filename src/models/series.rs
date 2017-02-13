ab07d9a281ccb45466532d8c22498766e4df5650
David Barnett <david@barnett.net.nz>
2017-02-14 12:21:37 +1300

    add tests for series model
    series models now implement default trait for easier testing

--- src/models/series.rs
+++ src/models/series.rs
@@ -2,7 +2,7 @@ use ::schema::*;
 use ::error::*;
 use chrono::NaiveDate;
 
-#[derive(Queryable, Serialize, Deserialize, Debug)]
+#[derive(Queryable, Serialize, Deserialize, Debug, Default)]
 pub struct Series {
     pub id: i32,
     pub title: String,
@@ -14,7 +14,7 @@ pub struct Series {
 }
 
 
-#[derive(Insertable, Serialize, Deserialize, Debug)]
+#[derive(Insertable, Serialize, Deserialize, Debug, Default)]
 #[table_name = "series"]
 pub struct NewSeries {
     pub title: String,
@@ -25,7 +25,7 @@ pub struct NewSeries {
     pub info_link: Option<String>,
 }
 
-#[derive(FromForm, Serialize, Deserialize, Debug)]
+#[derive(FromForm, Serialize, Deserialize, Debug, Default)]
 pub struct SeriesForm {
     pub title: String,
     pub start_date: Option<String>,
@@ -43,8 +43,8 @@ impl NewSeries {
 
             if end < start {
                 return Err(ErrorKind::InvalidForm("series".to_owned(),
-                                                  "end date is before start date".to_owned())
-                    .into());
+                "end date is before start date".to_owned())
+                           .into());
             }
         }
 
@@ -68,5 +68,94 @@ impl From<SeriesForm> for NewSeries {
             episodes_current: form.episodes_current,
             info_link: form.info_link,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use chrono::NaiveDate;
+
+    #[test]
+    fn newseries_validate_end_before_start_date() {
+        let series = NewSeries {
+            start_date: Some(NaiveDate::from_ymd(2017, 1, 1)),
+            end_date: Some(NaiveDate::from_ymd(2017, 2, 1)),
+            .. Default::default()
+        };
+
+        assert!(series.validate().is_ok());
+    }
+
+    #[test]
+    fn newseries_validate_end_after_start_date() {
+        let series = NewSeries {
+            start_date: Some(NaiveDate::from_ymd(2017, 2, 1)),
+            end_date: Some(NaiveDate::from_ymd(2017, 1, 1)),
+            .. Default::default()
+        };
+
+        assert!(series.validate().is_err());
+    }
+
+    #[test]
+    fn newseries_validate_end_on_start_date() {
+        let series = NewSeries {
+            start_date: Some(NaiveDate::from_ymd(2017, 1, 1)),
+            end_date: Some(NaiveDate::from_ymd(2017, 1, 1)),
+            .. Default::default()
+        };
+
+        assert!(series.validate().is_ok());
+    }
+
+    #[test]
+    fn seriesform_to_newseries_date_parse_start_date_valid() {
+        let series_form = SeriesForm {
+            start_date: Some("2017-01-01".to_owned()),
+            .. Default::default()
+        };
+
+        let series: NewSeries = series_form.into();
+
+        assert!(series.start_date.is_some());
+        assert_eq!(NaiveDate::from_ymd(2017, 1, 1), series.start_date.unwrap());
+    }
+
+    #[test]
+    fn seriesform_to_newseries_date_parse_start_date_invalid() {
+        let series_form = SeriesForm {
+            start_date: Some("invalid date".to_owned()),
+            .. Default::default()
+        };
+
+        let series: NewSeries = series_form.into();
+
+        assert!(series.start_date.is_none());
+    }
+
+    #[test]
+    fn seriesform_to_newseries_date_parse_end_date_valid() {
+        let series_form = SeriesForm {
+            end_date: Some("2017-01-01".to_owned()),
+            .. Default::default()
+        };
+
+        let series: NewSeries = series_form.into();
+
+        assert!(series.end_date.is_some());
+        assert_eq!(NaiveDate::from_ymd(2017, 1, 1), series.end_date.unwrap());
+    }
+
+    #[test]
+    fn seriesform_to_newseries_date_parse_end_date_invalid() {
+        let series_form = SeriesForm {
+            end_date: Some("invalid date".to_owned()),
+            .. Default::default()
+        };
+
+        let series: NewSeries = series_form.into();
+
+        assert!(series.end_date.is_none());
     }
 }
