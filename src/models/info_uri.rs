a2a84c6e2c0e4b305f15f7a0edf7ddd9bf45d1f2
David Barnett <david@barnett.net.nz>
2017-03-01 14:45:53 +1300

    fix info uri models not having pub fields

--- src/models/info_uri.rs
+++ src/models/info_uri.rs
@@ -5,24 +5,24 @@ use models::Series;
 #[table_name="info_uri"]
 #[belongs_to(Series, foreign_key="series_id")]
 pub struct InfoUri {
-    id: i32,
-    series_id: i32,
-    uri: String,
-    primary: bool,
+    pub id: i32,
+    pub series_id: i32,
+    pub uri: String,
+    pub primary: bool,
 }
 
 #[derive(Insertable, Serialize, Deserialize, Debug, Default)]
 #[table_name="info_uri"]
 pub struct NewInfoUri {
-    series_id: i32,
-    uri: String,
-    primary: bool,
+    pub series_id: i32,
+    pub uri: String,
+    pub primary: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct InfoUriForm {
-    uri: String,
-    primary: Option<bool>,
+    pub uri: String,
+    pub primary: Option<bool>,
 }
 
 impl InfoUriForm {
