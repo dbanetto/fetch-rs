c7deb19a927890c9a5c555e01e19b66e7becb8b0
David Barnett <david@barnett.net.nz>
2017-04-08 19:23:41 +1200

    remove template and only serve static files

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,32 +2,22 @@ use rocket::Rocket;
 
 use std::path::{PathBuf, Path};
 use rocket::response::NamedFile;
-use rocket_contrib::Template;
-use std::collections::HashMap;
-use serde_json::Value;
 use rocket::Route;
 
 mod api;
 
 #[get("/")]
-fn root() -> Template {
-    let context: HashMap<String, Value> = HashMap::new();
-    Template::render("index", &context)
+fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("public").join("index.html")).ok()
 }
 
 #[get("/<path..>")]
-fn index(path: Option<PathBuf>) -> Template {
-    let context: HashMap<String, Value> = HashMap::new();
-    Template::render("index", &context)
-}
-
-#[get("/public/<file..>")]
-fn static_files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("public/").join(file)).ok()
+fn static_files(path: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("public").join(path)).ok()
 }
 
 fn routes() -> Vec<Route> {
-    routes![static_files, index, root]
+    routes![index, static_files]
 }
 
 pub fn mount(rocket: Rocket) -> Rocket {
