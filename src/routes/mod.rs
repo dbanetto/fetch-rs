e0d1518866a916e1747fb2ab7b2d2f945e0710e7
David Barnett <david@barnett.net.nz>
2017-04-08 19:32:06 +1200

    serve index as static file

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,23 +2,18 @@ use rocket::Rocket;
 
 use std::path::{PathBuf, Path};
 use rocket::response::NamedFile;
-use rocket_contrib::Template;
-use std::collections::HashMap;
-use serde_json::Value;
 use rocket::Route;
 
 mod api;
 
 #[get("/")]
-fn root() -> Template {
-    let context: HashMap<String, Value> = HashMap::new();
-    Template::render("index", &context)
+fn root() -> Option<NamedFile> {
+    NamedFile::open(Path::new("public").join("index.html")).ok()
 }
 
-#[get("/<path..>")]
-fn index(path: Option<PathBuf>) -> Template {
-    let context: HashMap<String, Value> = HashMap::new();
-    Template::render("index", &context)
+#[get("/series/<path..>")]
+fn series(path: Option<PathBuf>) -> Option<NamedFile> {
+    NamedFile::open(Path::new("public").join("index.html")).ok()
 }
 
 #[get("/public/<file..>")]
@@ -27,7 +22,7 @@ fn static_files(file: PathBuf) -> Option<NamedFile> {
 }
 
 fn routes() -> Vec<Route> {
-    routes![static_files, index, root]
+    routes![static_files, series, root]
 }
 
 pub fn mount(rocket: Rocket) -> Rocket {
