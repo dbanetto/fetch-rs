d7d033d72a84a7f49e371c164fb5818eb952df33
David Barnett <david@barnett.net.nz>
2018-01-06 15:07:31 +1300

    Port GET /series/ to iron

--- src/routes/api/mod.rs
+++ src/routes/api/mod.rs
@@ -1,4 +1,4 @@
-// pub mod series;
+pub mod series;
 // pub mod info_uri;
 
 use iron::prelude::*;
@@ -6,33 +6,33 @@ use mount::Mount;
 use util::ApiResult;
 use serde_json;
 use iron::status::Status;
-use hyper::header::ContentType;
-use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
- 
+use hyper::mime::{Attr, Mime, SubLevel, TopLevel, Value};
+
 
 fn index() -> ApiResult<String, String> {
     ApiResult::ok("API available".to_owned())
 }
 
 fn handle_index(_: &mut Request) -> IronResult<Response> {
-
     let result = index();
-    let mut resp = Response::new();
-    resp.headers.set(
-        ContentType(Mime(TopLevel::Application, SubLevel::Json,
-                         vec![(Attr::Charset, Value::Utf8)]))
-        );
 
-    resp.status = if result.success {
-        Some(Status::Ok)
+    let status = if result.success {
+        Status::Ok
     } else {
-        Some(Status::InternalServerError)
+        Status::InternalServerError
     };
 
     let bytes = serde_json::to_vec(&result).unwrap();
-    resp.body = Some(Box::new(bytes));
 
-    Ok(resp)
+    Ok(Response::with((
+        status,
+        bytes,
+        Mime(
+            TopLevel::Application,
+            SubLevel::Json,
+            vec![(Attr::Charset, Value::Utf8)],
+        ),
+    )))
 }
 
 pub fn routes() -> Mount {
@@ -43,5 +43,6 @@ pub fn routes() -> Mount {
     );
 
     mount.mount("/", router);
+    mount.mount("/series/", series::routes());
     mount
 }
--- src/routes/api/series.rs
+++ src/routes/api/series.rs
@@ -1,140 +1,170 @@
-
+use iron::prelude::*;
+use router::Router;
 use std::error::Error;
-use db::DB;
+use db::DbConnection;
 use util::ApiResult;
-use rocket::Route;
-use rocket_contrib::Json;
 use models::*;
 use diesel::prelude::*;
 use schema::{info_uri, series};
-use super::info_uri::{new_uri, update_uri};
-use diesel::{insert, update, delete};
+// use super::info_uri::{new_uri, update_uri};
+use diesel::{delete, insert, update};
 use serde_json;
+use iron::status::Status;
+use hyper::mime::{Attr, Mime, SubLevel, TopLevel, Value};
+use diesel::pg::PgConnection;
 
-#[get("/")]
-fn all(db: DB) -> Json<ApiResult<Vec<Series>, String>> {
-    let conn = db.conn();
+fn all(db: &PgConnection) -> ApiResult<Vec<Series>, String> {
+    let conn = &db;
 
-    let result = series::dsl::series.load::<Series>(conn);
+    let result = series::dsl::series.load::<Series>(*conn);
 
-    ApiResult::new(result.map_err(|e| e.description().to_owned())).json()
+    ApiResult::new(result.map_err(|e| e.description().to_owned()))
 }
 
-#[get("/<series_id>")]
-fn select(db: DB, series_id: i32) -> Json<ApiResult<Series, String>> {
-    let conn = db.conn();
-
-    let series: Series = match series::dsl::series
-        .filter(series::id.eq(series_id))
-        .select(series::all_columns)
-        .first(conn) {
-        Ok(s) => s,
-        Err(e) => return ApiResult::err_format(e).json(),
+fn handle_all(req: &mut Request) -> IronResult<Response> {
+    let conn = match req.extensions.get::<DbConnection>().unwrap().get() {
+        Ok(conn) => conn,
+        Err(err) => return Err(IronError::new(err, Status::RequestTimeout)),
     };
 
-    ApiResult::ok(series).json()
-}
+    let result = all(&*conn);
 
-#[post("/new", data = "<series_form>")]
-fn new(db: DB, series_form: Json<SeriesForm>) -> Json<ApiResult<serde_json::Value, String>> {
-    let series_form = series_form.into_inner();
-    let (new_series, info_uris) = series_form.into_new();
-
-    if let Err(e) = new_series.validate() {
-        return ApiResult::err_format(e).json();
-    }
-
-    let conn = db.conn();
-
-    let new_series: Series = match insert(&new_series)
-        .into(series::table)
-        .returning(series::all_columns)
-        .get_result(conn) {
-        Ok(s) => s,
-        Err(e) => return ApiResult::err_format(e).json(),
+    let status = if result.success {
+        Status::Ok
+    } else {
+        Status::InternalServerError
     };
 
-    let new_info_uris: Vec<InfoUri> = match info_uris {
-        Some(uris) => {
-            let info_uris = uris.into_iter()
-                .map(|i| i.into_insertable(&new_series))
-                .collect::<Vec<NewInfoUri>>();
-            match insert(&info_uris)
-                .into(info_uri::table)
-                .returning(info_uri::all_columns)
-                .get_results(conn) {
-                Ok(uris) => uris,
-                Err(e) => return ApiResult::err_format(e).json(),
-            }
-        }
-        None => Vec::new(),
-    };
+    let bytes = serde_json::to_vec(&result).unwrap();
 
-    let mut result = serde_json::to_value(new_series).unwrap();
-    result.as_object_mut().unwrap().insert(
-        "info_uri".to_owned(),
-        serde_json::to_value(new_info_uris).unwrap(),
-    );
+    let resp = Response::with((
+        bytes,
+        status,
+        Mime(
+            TopLevel::Application,
+            SubLevel::Json,
+            vec![(Attr::Charset, Value::Utf8)],
+        ),
+    ));
 
-    ApiResult::ok(result).json()
+
+    Ok(resp)
 }
 
-#[put("/<series_id>", data = "<series_form>")]
-fn update_series(
-    db: DB,
-    series_id: i32,
-    series_form: Json<SeriesForm>,
-) -> Json<ApiResult<Series, String>> {
-    let conn = db.conn();
+// #[get("/<series_id>")]
+// fn select(db: DB, series_id: i32) -> Json<ApiResult<Series, String>> {
+//     let conn = db.conn();
 
-    let (series_put, info_uris) = series_form.into_inner().into_new();
+//     let series: Series = match series::dsl::series
+//         .filter(series::id.eq(series_id))
+//         .select(series::all_columns)
+//         .first(conn) {
+//         Ok(s) => s,
+//         Err(e) => return ApiResult::err_format(e).json(),
+//     };
 
-    if let Err(e) = series_put.validate() {
-        return ApiResult::err_format(e).json();
-    }
+//     ApiResult::ok(series).json()
+// }
 
-    let series: Series = match update(series::dsl::series.filter(series::id.eq(series_id)))
-        .set((
-            series::title.eq(series_put.title),
-            series::poster_url.eq(series_put.poster_url),
-        ))
-        .returning(series::all_columns)
-        .get_result(conn) {
-        Ok(s) => s,
-        Err(e) => return ApiResult::err_format(e).json(),
-    };
+// #[post("/new", data = "<series_form>")]
+// fn new(db: DB, series_form: Json<SeriesForm>) -> Json<ApiResult<serde_json::Value, String>> {
+//     let series_form = series_form.into_inner();
+//     let (new_series, info_uris) = series_form.into_new();
 
-    if let Some(uris) = info_uris {
-        for uri in uris {
-            if uri.id.is_some() {
-                // update
-                update_uri(&db, series_id, uri);
-            } else {
-                // create
-                new_uri(&db, series_id, uri);
-            }
-        }
-    }
+//     if let Err(e) = new_series.validate() {
+//         return ApiResult::err_format(e).json();
+//     }
 
-    ApiResult::ok(series).json()
-}
+//     let conn = db.conn();
 
-#[delete("/<series_id>")]
-fn delete_series(db: DB, series_id: i32) -> Json<ApiResult<Series, String>> {
-    let conn = db.conn();
+//     let new_series: Series = match insert(&new_series)
+//         .into(series::table)
+//         .returning(series::all_columns)
+//         .get_result(conn) {
+//         Ok(s) => s,
+//         Err(e) => return ApiResult::err_format(e).json(),
+//     };
 
-    match delete(series::dsl::series.filter(series::id.eq(series_id)))
-        .returning(series::all_columns)
-        .get_result(conn) {
-        Ok(result) => ApiResult::ok(result).json(),
-        Err(e) => ApiResult::err_format(e).json(),
-    }
-}
+//     let new_info_uris: Vec<InfoUri> = match info_uris {
+//         Some(uris) => {
+//             let info_uris = uris.into_iter()
+//                 .map(|i| i.into_insertable(&new_series))
+//                 .collect::<Vec<NewInfoUri>>();
+//             match insert(&info_uris)
+//                 .into(info_uri::table)
+//                 .returning(info_uri::all_columns)
+//                 .get_results(conn) {
+//                 Ok(uris) => uris,
+//                 Err(e) => return ApiResult::err_format(e).json(),
+//             }
+//         }
+//         None => Vec::new(),
+//     };
+
+//     let mut result = serde_json::to_value(new_series).unwrap();
+//     result.as_object_mut().unwrap().insert(
+//         "info_uri".to_owned(),
+//         serde_json::to_value(new_info_uris).unwrap(),
+//     );
+
+//     ApiResult::ok(result).json()
+// }
+
+// #[put("/<series_id>", data = "<series_form>")]
+// fn update_series(
+//     db: DB,
+//     series_id: i32,
+//     series_form: Json<SeriesForm>,
+// ) -> Json<ApiResult<Series, String>> {
+//     let conn = db.conn();
+
+//     let (series_put, info_uris) = series_form.into_inner().into_new();
+
+//     if let Err(e) = series_put.validate() {
+//         return ApiResult::err_format(e).json();
+//     }
+
+//     let series: Series = match update(series::dsl::series.filter(series::id.eq(series_id)))
+//         .set((
+//             series::title.eq(series_put.title),
+//             series::poster_url.eq(series_put.poster_url),
+//         ))
+//         .returning(series::all_columns)
+//         .get_result(conn) {
+//         Ok(s) => s,
+//         Err(e) => return ApiResult::err_format(e).json(),
+//     };
+
+//     if let Some(uris) = info_uris {
+//         for uri in uris {
+//             if uri.id.is_some() {
+//                 // update
+//                 update_uri(&db, series_id, uri);
+//             } else {
+//                 // create
+//                 new_uri(&db, series_id, uri);
+//             }
+//         }
+//     }
+
+//     ApiResult::ok(series).json()
+// }
+
+// #[delete("/<series_id>")]
+// fn delete_series(db: DB, series_id: i32) -> Json<ApiResult<Series, String>> {
+//     let conn = db.conn();
+
+//     match delete(series::dsl::series.filter(series::id.eq(series_id)))
+//         .returning(series::all_columns)
+//         .get_result(conn) {
+//         Ok(result) => ApiResult::ok(result).json(),
+//         Err(e) => ApiResult::err_format(e).json(),
+//     }
+// }
 
 
-pub fn routes() -> Vec<Route> {
-    use routes::api::info_uri::routes as uri_routes;
-    let mut routes = routes![all, new, select, update_series, delete_series];
-    routes.append(&mut uri_routes());
-    routes
+pub fn routes() -> Router {
+    router!(
+        index: get "/" => handle_all,
+        )
 }
