9f5b0e096cbd3bd92a8d0d55be635d2c04268759
David Barnett <david@barnett.net.nz>
2018-01-13 11:46:24 +1300

    Add status code and successful requests to logging

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,14 +1,26 @@
 use iron::prelude::*;
-use iron::middleware::{BeforeMiddleware, AfterMiddleware};
+use iron::middleware::{AfterMiddleware, BeforeMiddleware};
 
-use ::db::DbConnection;
+use db::DbConnection;
 
 pub struct ErrorLog;
 
-
 impl AfterMiddleware for ErrorLog {
+    fn after(&self, req: &mut Request, res: Response) -> IronResult<Response> {
+        let status = match res.status {
+            Some(status) => format!("{}", status.to_u16()),
+            None => "OK".to_owned(),
+        };
+        println!("{} {} {}", status, req.method, req.url);
+        Ok(res)
+    }
+
     fn catch(&self, req: &mut Request, err: IronError) -> IronResult<Response> {
-        println!("ERROR {} {}: {}", req.method, req.url, err);
+        let status = match err.response.status {
+            Some(status) => format!("{}", status.to_u16()),
+            None => "ERROR".to_owned(),
+        };
+        println!("{} {} {}: {}", status, req.method, req.url, err);
         Err(err)
     }
 }
