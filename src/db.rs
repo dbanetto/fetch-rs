f815c799cedd46873c2be8057cf6730ddf8213e2
David Barnett <david@barnett.net.nz>
2018-01-16 18:48:24 +1300

    Update to diesel 1.1 and remove r2d2_diesel

--- src/db.rs
+++ src/db.rs
@@ -3,22 +3,23 @@ use std::env;
 use iron::typemap;
 use dotenv::dotenv;
 use diesel::pg::PgConnection;
-use r2d2_diesel::ConnectionManager;
-use r2d2::{Pool, Config};
+use diesel::r2d2::ConnectionManager;
+use r2d2::Pool;
 
 pub struct DbConnection {
     pub pool: Pool<ConnectionManager<PgConnection>>,
 }
 
-impl typemap::Key for DbConnection { type Value = Pool<ConnectionManager<PgConnection>>; }
+impl typemap::Key for DbConnection {
+    type Value = Pool<ConnectionManager<PgConnection>>;
+}
 
 pub fn get_pool() -> DbConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
-    let config = Config::default();
     let manager = ConnectionManager::new(database_url);
-    let pool = Pool::new(config, manager).expect("Failed to create pool.");
+    let pool = Pool::new(manager).expect("Failed to create pool.");
 
     DbConnection { pool: pool }
 }
