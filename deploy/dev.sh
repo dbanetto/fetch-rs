5337046cb7c775207a9ac92bf4f71315d6c474f4
David Barnett <david@barnett.net.nz>
2018-05-28 19:16:59 +1200

    Move dockerfiles to service directories

--- deploy/dev.sh
+++ deploy/dev.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
 cd `dirname $0`
+OPTION=$1
 
-( cd ../services/web && npm run watch ) &
-( cd ../services/api && cargo watch -x build --ignore target/ --ignore migrations/) &
+function print_help {
+    echo "dev.sh build - build containers"
+    echo "dev.sh run   - run the dev environment"
+}
 
-docker-compose -p fetch -f docker-compose-dev.yml $@
+case $OPTION in
+    build)
+        docker-compose -p fetch -f docker-compose-dev.yml build
+        ;;
+    up|start|run)
+        (cd ../services/web && npm run watch) &
+        (cd ../services/api && cargo watch -x build --ignore target/ --ignore migrations/) &
+        (docker-compose -p fetch -f docker-compose-dev.yml up)
+        ;;
+    stop)
+        docker-compose -p fetch -f docker-compose-dev.yml stop
+        ;;
+    *)
+        print_help
+esac
