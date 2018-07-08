31dce42397681fd4721e3631e8d703354a5f0b11
David Barnett <david@barnett.net.nz>
2018-07-08 20:14:25 +1200

    Deploy: Fallback to image binary

--- deploy/docker/api/watch-dev.sh
+++ deploy/docker/api/watch-dev.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 apt-get install -yqq entr
-ls /opt | entr -r /opt/fetch-api --config /etc/fetch.toml
+echo "/bin/fetch-api\n/opt/fetch-api" | entr -r bash -xec 'if [ -f /opt/fetch-api ] ; then /opt/fetch-api --config /etc/fetch.toml ; else /bin/fetch-api --config /etc/fetch.toml ; fi'
