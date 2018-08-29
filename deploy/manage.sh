2f5968140252b99a5f79dc4084f02b046df1b177
David Barnett <david@barnett.net.nz>
2018-08-29 21:19:13 +1200

    Fix API docker build
    Add UAT docker-compose file and update ignores

--- deploy/manage.sh
+++ deploy/manage.sh
@@ -22,6 +22,32 @@ case $OPTION in
     start|run)
         docker-compose -p fetch -f docker/docker-compose-dev.yml up
         ;;
+    uat)
+        case $2 in
+            up)
+                docker-compose -p fetch_uat -f docker/docker-compose-uat.yml up
+                ;;
+            build)
+                docker-compose -p fetch_uat -f docker/docker-compose-uat.yml build
+                ;;
+            stop)
+                docker-compose -p fetch_uat -f docker/docker-compose-uat.yml stop
+                ;;
+        esac
+        ;;
+    prod)
+        case $2 in
+            up)
+                docker-compose -p fetch_prod -f docker/docker-compose-prod.yml up
+                ;;
+            pull)
+                docker-compose -p fetch_prod -f docker/docker-compose-prod.yml pull
+                ;;
+            stop)
+                docker-compose -p fetch_prod -f docker/docker-compose-prod.yml stop
+                ;;
+        esac
+        ;;
     backup)
         SERVER=${2:-"$DEFAULT_SEVER"}
         ./backup/fetch-backup backup "$SERVER"
