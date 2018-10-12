05b97fbd4f61b7f14cb99a7b5530f08703385ac9
David Barnett <david@barnett.net.nz>
2018-10-13 11:39:06 +1300

    web: display results of fetch request

--- services/web/src/pages/fetch.tsx
+++ services/web/src/pages/fetch.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import { callFetch, getStatus } from "../actions/fetch";
 import { getAllSeries } from "../actions/series";
@@ -29,25 +30,30 @@ class Fetch extends React.PureComponent<IFetchProps> {
     }
 
     public render() {
-        return (
-            <div className="container box">
-                <div className="is-flex">
-                    <h1 className="title">Fetch</h1>
-                    {this.renderStatus()}
-                </div>
-                <div>
-                    <a
-                        className="button is-success"
-                        href="javascript:void(0)"
-                        onClick={this.handleFetch}
-                    >
-                        Fetch
-                    </a>
-
-                    {this.renderResults()}
-                </div>
+      return (
+        <div className="container box">
+          <div className="is-flex is-flex-horizontal is-flex-spacebetween">
+            <div className="is-flex">
+              <h1 className="title">Fetch</h1>
             </div>
-        );
+            <div>
+              <a
+                className={`button is-success ${this.props.loading ? "is-loading" : ""}`}
+                href="javascript:void(0)"
+                onClick={this.handleFetch}
+              >
+                Fetch
+              </a>
+            </div>
+          </div>
+          <div>
+            {this.renderStatus()}
+          </div>
+          <div>
+            {this.renderResults()}
+          </div>
+        </div>
+      );
     }
 
     private handleFetch() {
@@ -56,36 +62,58 @@ class Fetch extends React.PureComponent<IFetchProps> {
     }
 
     private renderResults() {
-        const shows = this.props.series;
-        return this.props.results.map((result, n) => {
-            const show = this.props.series.find((s) => s.id === result.id);
-            return (<div key={`${result.id}-${n}`}>
-                {show.title} - {result.success} - {result.found} - {result.count}
-            </div>);
-            });
+      const shows = this.props.series;
+      return this.props.results.map((result, n) => {
+        const show = this.props.series.find((s) => s.id === result.id);
+        const success = result.success ? "success" : "failed";
+        const found = result.found ? `found new, now up to ${result.count}` : "nothing new";
+        return [
+          (<hr key={n} />),
+          (<div
+            key={`${result.id}-${n}`}
+            className="is-flex"
+          >
+            <Link to={`/series/${ show.id }`} >
+              <div className="poster">
+                <img className="image" src={show.poster_url} />
+              </div>
+            </Link>
+            <div>
+              <div>
+                <h2 className="title">{show.title}</h2>
+              </div>
+              <div>
+                {success} - {found}
+              </div>
+            </div>
+          </div>
+          ),
+        ]});
     }
 
     private renderStatus() {
-        return [
-            "api",
-            "transmission",
-        ].map(
-            (s, n) => {
-            const service = this.props.status[s];
-            let colour = "unknown";
-            let title = `${s} - unknown`;
-            if (service) {
-                colour = service.status ? "ok" : "err";
-                title = `${s} - ${service.status ? "up" : `down - ${service.message}`}`;
-            }
-            return (
-                <div
-                    key={n}
-                    title={title}
-                    className={`status-circle status-${colour}`}
-                />
-            );
-});
+      const statuses = [ "api", "transmission" ].map(
+        (s, n) => {
+          const service = this.props.status[s];
+          let colour = "is-static";
+          let title = `${s} - unknown`;
+          if (service) {
+            colour = service.status ? "is-success" : "is-danger";
+            title = `${s} - ${service.status ? "up" : `down - ${service.message}`}`;
+          }
+          return (<span
+            key={n}
+            title={title}
+            className={`button is-small ${colour} ${this.props.loading ? "is-loading" : ""}`}
+          >
+            {s}
+          </span>);
+        });
+
+      return (
+        <div className="buttons has-addons">
+          {statuses}
+        </div>);
     }
 }
 
